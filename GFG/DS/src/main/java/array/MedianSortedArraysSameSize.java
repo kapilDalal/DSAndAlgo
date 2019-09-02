2f9677c8481c13ebd451336d9563ee7ef09472ba
Kapil Dalal <Kapildalal46@gmail.com>
2019-09-02 10:19:32 +0530

    array revision

--- GFG/DS/src/main/java/array/MedianSortedArraysSameSize.java
+++ GFG/DS/src/main/java/array/MedianSortedArraysSameSize.java
@@ -45,11 +45,45 @@ public class MedianSortedArraysSameSize {
 	}
 	
 	//gfg second method is better than the one implemented here
+	static int medianOfTwoArrays(Integer[] arr1,Integer[] arr2,int start1,int end1,int start2,int end2){
+		
+		int median =-1;
+		
+		if(end1-start1==0)
+			return (arr1[0]+arr2[0])/2;
+		
+		if(end1-start1==1)
+			return (Math.max(arr1[start1], arr2[start2])+Math.min(arr1[end1], arr2[end2]))/2;
+		
+		
+		int m1 = getMedian27Aug(arr1, start1, end1);
+		int m2 = getMedian27Aug(arr2, start2, end2);
+		
+		if(m1==m2)
+			return m1;
+		
+		
+		
+		if(m1<m2)
+			median = medianOfTwoArrays(arr1, arr2, (start1+end1)/2, end1, start2, (start2+end2)/2);
+		else
+			median = medianOfTwoArrays(arr1, arr2, start1, (start1+end1)/2, (start2+end2)/2, end2);
+		
+		return median;
+	}
+	
+	
+	static int getMedian27Aug(Integer[] arr1,int start,int end){
+		return arr1[(start+end)/2];
+	}
+	
 
 	public static void main(String[] args) {
 		Integer[] arr1 = { 1, 12, 15, 26, 38 };
 		Integer[] arr2 = { 2, 13, 17, 30, 45};
 		int median = getMedian(arr1, arr2);
+		
+		System.out.println(medianOfTwoArrays(arr1, arr2, 0, arr1.length-1, 0, arr2.length-1));
 		System.out.println(median);
 	}
 }
--- GFG/DS/src/main/java/array/MergeNArrayInMPlusN.java
+++ GFG/DS/src/main/java/array/MergeNArrayInMPlusN.java
@@ -44,11 +44,46 @@ public class MergeNArrayInMPlusN {
 		moveToEnd(arr1);
 		merge(arr1, arr2, arr1.length - arr2.length, arr2.length);
 	}
+	
+	static void moveToEnd26Aug(Integer[] arr,int sizeOfOtherArr){
+		for(int i=0;i<=arr.length-sizeOfOtherArr;i++){
+			int temp = -1;
+			if (arr[i] != -1) {
+				temp = arr[i];
+				for (int j = i; j < arr.length - 1; j++)
+					arr[j] = arr[j + 1];
+				arr[arr.length - 1] = temp;
+			}
+		}
+	}
+	static void merge26Aug(Integer[] arr1,Integer[] arr2){
+		int i = arr1.length-arr2.length+1;
+		int k=0;
+		int j=0;
+		while(i<arr1.length && j<arr2.length){
+			if(arr1[i]<arr2[j]){				
+				arr1[k] = arr1[i];				
+				i++;
+			}
+			else{
+				arr1[k] = arr2[j];				
+				j++;
+			}
+			k++;
+		}
+		while(j<arr2.length)
+			arr1[k++]=arr2[j++];
+		
+	}
+	static void mergeNToMPlusN26Aug(Integer[] arr1,Integer[] arr2){
+		moveToEnd26Aug(arr1, arr2.length);		
+		merge26Aug(arr1, arr2);
+	}
 
 	public static void main(String[] args) {
 		Integer[] arr1 = { 2, -1, 7, -1, -1, 10, -1 };
 		Integer[] arr2 = { 5, 8, 12, 14 };
-		mergeNToMPlusN(arr1, arr2);
+		mergeNToMPlusN26Aug(arr1, arr2);
 		for (int i = 0; i < arr1.length; i++)
 			System.out.print(arr1[i] + "\t");
 	}
