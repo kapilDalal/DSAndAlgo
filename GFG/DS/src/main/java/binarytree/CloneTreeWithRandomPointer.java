7da7107f4441f82d1e3285f0679d12f4b8b5393b
Kapil Dalal <Kapildalal46@gmail.com>
2019-09-19 11:16:18 +0530

    added second method i.e clone tree by modifying original tree

--- GFG/DS/src/main/java/binarytree/CloneTreeWithRandomPointer.java
+++ GFG/DS/src/main/java/binarytree/CloneTreeWithRandomPointer.java
@@ -65,6 +65,55 @@ public class CloneTreeWithRandomPointer {
 		System.out.println();
 		inorder(node.right);
 	}
+	
+	static void modifyTree(Node node){
+		if(node==null)
+			return;
+		modifyTree(node.left);
+		modifyTree(node.right);
+		
+		Node leftChild = node.left;
+		node.left = new Node(node.data);
+		Node clone = node.left;
+		clone.left = leftChild;
+		
+		if(node.right!=null)
+			clone.right = node.right.left;
+		
+	}
+	
+	private static void setRandom(Node node) {
+		if(node==null)
+			return;
+		if(node.random!=null)
+			node.left.random = node.random.left;
+		if(node.left!=null)
+			setRandom(node.left.left);
+		setRandom(node.right);		
+	}
+	private static void takeOutCloneFromTree(Node root, Node clone) {
+		if(root==null)
+			return;
+		if(root.left!=null)
+			root.left = root.left.left;
+		if(clone.left!=null)
+			clone.left = clone.left.left;
+		takeOutCloneFromTree(root.left, clone.left);
+		takeOutCloneFromTree(root.right, clone.right);
+		
+	}
+	static Node cloneTreeByModifyingOriginalTree(Node root){
+		modifyTree(root);
+		setRandom(root);
+		Node clone = root.left;
+		takeOutCloneFromTree(root,clone);
+		
+		return clone;
+		
+	}
+	
+
+	
 
 	public static void main(String[] args) {
 		
@@ -89,14 +138,22 @@ public class CloneTreeWithRandomPointer {
         root1.right.right.random = root1.left.left.right;
         root1.left.left.right.random = root1;
         
-        Node clone = cloneTree(root1);
+       /* Node clone = cloneTree(root1);
         System.out.println(root1);
         System.out.println(clone);
         System.out.println();
         inorder(root1);
         System.out.println();
-        inorder(clone);
+        inorder(clone);*/
         
+        Node clone = cloneTreeByModifyingOriginalTree(root1);
+        inorder(root1);
+		System.out.println();
+		inorder(clone);
+		
+		System.out.println();
+		System.out.println(root1);
+		System.out.println(clone);
         
         
 
