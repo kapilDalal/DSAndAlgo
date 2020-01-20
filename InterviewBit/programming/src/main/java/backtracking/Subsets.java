9a630e658c5b4134567f3b45077d62aa103572cd
Kapil Dalal <Kapildalal46@gmail.com>
2020-01-20 13:17:39 +0530

    backtracking approach

--- InterviewBit/programming/src/main/java/backtracking/Subsets.java
+++ InterviewBit/programming/src/main/java/backtracking/Subsets.java
@@ -63,8 +63,30 @@ public class Subsets {
 		System.out.println(l);
 	}
 	
+	static public ArrayList<ArrayList<Integer>> subsetsUsingBacktracking(ArrayList<Integer> a) {
+		ArrayList<ArrayList<Integer>> output = new ArrayList<ArrayList<Integer>>();
+		output.add(new ArrayList<Integer>());
+		if (a.size() == 0)
+			return output;
+		Collections.sort(a);
+		generate(a, output, new ArrayList<Integer>(), 0);
+		return output;
+	}
+	
+	static public void generate(ArrayList<Integer> a, ArrayList<ArrayList<Integer>> output, ArrayList<Integer> temp, int index)
+	{
+		for (int i = index; i < a.size(); i++)
+		{
+			temp.add(a.get(i));
+			output.add(new ArrayList<Integer>(temp));
+			generate(a, output, temp, i+1);
+			temp.remove(temp.size() - 1);
+		}
+	}
+	
 	public static void main(String[] args) {
-		subsets(new ArrayList<Integer>(Arrays.asList(12,13)));
+		//subsets(new ArrayList<Integer>(Arrays.asList(1,2,3)));		
+		System.out.println(subsetsUsingBacktracking(new ArrayList<Integer>(Arrays.asList(1,2,3))));
 	}
 
 }
