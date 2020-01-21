2d34dc32eac35402eb329be37e563d061fe6336c
Kapil Dalal <Kapildalal46@gmail.com>
2020-01-21 10:10:13 +0530

    editorial approach for combination sum

--- InterviewBit/programming/src/main/java/backtracking/CombinationSum.java
+++ InterviewBit/programming/src/main/java/backtracking/CombinationSum.java
@@ -9,12 +9,12 @@ public class CombinationSum {
 	static public ArrayList<ArrayList<Integer>> combinationSum(ArrayList<Integer> A, int B) {
 		ArrayList<ArrayList<Integer>> res = new ArrayList<>();
 		if(B==0){
-			res.add(new ArrayList<Integer>());
+			//res.add(new ArrayList<Integer>());
 			return res;
 		}
 		Collections.sort(A);
 		generate(A,B,0,new ArrayList<Integer>(),res);
-		System.out.println(res);
+		//System.out.println(res);
 		return res;
     }
 
@@ -36,6 +36,47 @@ public class CombinationSum {
 		}
 		
 	}
+	
+	public ArrayList<ArrayList<Integer>> combinationSumBT(ArrayList<Integer> input, int target) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        
+        if (target == 0) {
+            return ans;
+        }
+        
+        Collections.sort(input);
+        combinationR(input, ans, target, new ArrayList<>(), 0, 0);
+        return ans;
+    }
+    
+    private void combinationR(ArrayList<Integer> input, ArrayList<ArrayList<Integer>> ans, int target, ArrayList<Integer> combination, int sum, int start) {
+        if (input.get(start) > target) {
+            // Backtrack, as you will never find combination here
+            return;
+        }
+        
+        for (int i = start; i < input.size(); i++) {
+            if (i != 0 && input.get(i) == input.get(i - 1)) {
+                // Avoid duplicates, if adjacent is equal then skip that value, as it will lead to duplicates
+                continue;
+            }            
+            
+            if (sum + input.get(i) == target) {
+                combination.add(input.get(i));
+                ans.add(new ArrayList<>(combination));
+                combination.remove(combination.size() - 1);
+                break;
+            } else if (sum + input.get(i) < target) {
+                combination.add(input.get(i));
+                combinationR(input, ans, target, combination, sum + input.get(i), i);
+                combination.remove(combination.size() - 1);
+            } else {
+                // As the input is sorted, if current sum + input.get(i) exceeds the target, rest are obviously going to exceed it. So skip the unnecessary work
+                // No more combinations possible
+                break;
+            }
+        }
+    } 
 	public static void main(String[] args) {
 		combinationSum(new ArrayList<Integer>(Arrays.asList(2,3,6,7)), 0);
 	}
