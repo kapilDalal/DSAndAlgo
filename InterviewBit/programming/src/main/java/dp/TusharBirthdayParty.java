9190eea247e5552ece8c90991eac2f05652bd150
Kapil Dalal <Kapildalal46@gmail.com>
2020-03-11 09:06:46 +0530

    added dp method for tushar's birthday party

--- InterviewBit/programming/src/main/java/dp/TusharBirthdayParty.java
+++ InterviewBit/programming/src/main/java/dp/TusharBirthdayParty.java
@@ -31,9 +31,44 @@ public class TusharBirthdayParty {
 			calPrice(wt, val, w-wt.get(i), p+val.get(i));
 		}
 	}
+	
+	public int solveUsingDP(final List<Integer> A, final List<Integer> B, final List<Integer> C) {
+        int totSum = 0;
+        
+        int a = 0;
+        
+        for(Integer b : A){
+            if(b>a){
+                a = b;
+            }
+        }
+        
+        int [] v = new int[a+1];
+        for(int i = 1;i<=a;i++){
+            int min = Integer.MAX_VALUE;
+            for(int j = 0;j<B.size();j++){
+                if(B.get(j)>i){
+                    continue;
+                }
+                int value = v[i-B.get(j)] + C.get(j);
+                if(value<min){
+                    min = value;
+                }
+            }
+            v[i] = min;
+        }
+        for(int i=0;i<v.length;i++)
+        	System.out.print(v[i]+"\t");
+        for(Integer b : A){        	
+            totSum += v[b];
+        }
+           
+        return totSum;
+    }
+	
 	public static void main(String[] args) {
 		TusharBirthdayParty tbp = new TusharBirthdayParty();
-		System.out.println(tbp.solve(Arrays.asList(4,6), Arrays.asList(1,3), Arrays.asList(5,3)));
+		System.out.println(tbp.solveUsingDP(Arrays.asList(4,6), Arrays.asList(1,3), Arrays.asList(5,3)));
 	}
 
 }
