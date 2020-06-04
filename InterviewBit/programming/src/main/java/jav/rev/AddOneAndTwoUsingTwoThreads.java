f1be899203bb5a5b4526ebf21cb6c99f9f611b9f
kapilDalal <kapildalal46@gmail.com>
2020-06-04 08:47:45 +0530

    moved synchronized block to appropraite place

--- InterviewBit/programming/src/main/java/jav/rev/AddOneAndTwoUsingTwoThreads.java
+++ InterviewBit/programming/src/main/java/jav/rev/AddOneAndTwoUsingTwoThreads.java
@@ -8,29 +8,36 @@ public class AddOneAndTwoUsingTwoThreads {
 	
 
 	public void addFirstThread() throws InterruptedException {
-		while (!firstThreadTurn) {
-			wait();
+		synchronized (this) {
+			while (!firstThreadTurn) {
+				wait();
+			}
+			if(i<10) {
+				i = i + 1;
+				System.out.println(Thread.currentThread().getName()+" "+i);
+			}
+			firstThreadTurn = false;
+			secondThreadTurn = true;
+			notify();
 		}
-		if(i<10) {
-			i = i + 1;
-			System.out.println(Thread.currentThread().getName()+" "+i);
-		}
-		firstThreadTurn = false;
-		secondThreadTurn = true;
-		notify();
+		
 	}
 
 	public void addSecondThread() throws InterruptedException {
-		while (!secondThreadTurn) {
-			wait();
+		
+		synchronized (this) {
+			while (!secondThreadTurn) {
+				wait();
+			}
+			if(i<10) {
+				i = i + 1;
+				System.out.println(Thread.currentThread().getName()+" "+i);
+			}
+			firstThreadTurn = true;
+			secondThreadTurn = false;
+			notify();
 		}
-		if(i<10) {
-			i = i + 1;
-			System.out.println(Thread.currentThread().getName()+" "+i);
-		}
-		firstThreadTurn = true;
-		secondThreadTurn = false;
-		notify();
+		
 	}
 
 	public  int getI() {
@@ -40,7 +47,7 @@ public class AddOneAndTwoUsingTwoThreads {
 	public static void main(String[] args) {
 		AddOneAndTwoUsingTwoThreads a = new AddOneAndTwoUsingTwoThreads();
 		Thread t1 = new Thread(() -> {
-			synchronized(a){
+			
 				while (a.getI() < 10){
 					try {
 						a.addFirstThread();
@@ -49,10 +56,10 @@ public class AddOneAndTwoUsingTwoThreads {
 						e.printStackTrace();
 					}
 				}
-			}
+			
 		});
 		Thread t2 = new Thread(() -> {
-			synchronized (a) {
+			
 				while (a.getI() < 10){
 					try {
 						a.addSecondThread();
@@ -61,7 +68,7 @@ public class AddOneAndTwoUsingTwoThreads {
 						e.printStackTrace();
 					}
 				}				
-			}
+			
 		});
 
 		t1.start();
